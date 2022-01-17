90c49a4d6b88e4a1a3878a1466fe2573422bf4a4
Shen Zhengyu <18302010009@fudan.edu.cn>
2022-01-17 11:30:04 +0800

    Integrate.

--- ooad_lab2/README.md
+++
@@ -1,2 +0,0 @@
-# Student Information Management System
-A system to manage the information of student, major and school.
--- ooad_lab2/src/main/java/com/ooad/lab2/repository/InstructingModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ooad.lab2.repository;
-
-import com.ooad.lab2.domain.InstructingModule;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstructingModuleRepository extends CrudRepository<InstructingModule, Long> {
-}
